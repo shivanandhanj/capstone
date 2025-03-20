7c1f27e54fc096573612ab30af8ef1d8abb43253
Shivanandhanj <shivnanzu@gmail.com>
2025-03-20 16:11:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u shivanandhanj -p dckr_pat_4_kq9frzhYwog05BSMAu6NC8n5o
+    docker login -u shivanandhanj -p dckr_pat_KjRZJc1jLclL34q91-uAuej1dxk
     docker tag test1 shivanandhanj/test1
     docker push shivanandhanj/test1
     # docker-compose up -d
